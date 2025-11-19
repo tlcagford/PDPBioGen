227d0dc1192a7b55b08ce98844343da949af2902
Tony E Ford <tlcagford@gmail.com>
2025-11-19 08:28:33 -0700

    Create Install Python and pip.sh

---
+++ Install Python and pip.sh
@@ -0,0 +1,7 @@
+# Install Python and pip
+sudo apt update
+sudo apt install -y python3 python3-pip python3-venv
+
+# Create virtual environment
+python3 -m venv pdpbiogen-env
+source pdpbiogen-env/bin/activate
