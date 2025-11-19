9545e92b757892e370493acbe31f5431770d2ca9
Tony E Ford <tlcagford@gmail.com>
2025-11-19 07:26:48 -0700

    Create logger.py

---
+++ pdpbiogen/logger.py
@@ -0,0 +1,29 @@
+import logging
+import sys
+
+def setup_logger(level=logging.INFO):
+    """Set up logging configuration."""
+    logger = logging.getLogger('pdpbiogen')
+    
+    # Avoid adding handlers multiple times
+    if logger.handlers:
+        return logger
+    
+    logger.setLevel(level)
+    
+    # Create formatter
+    formatter = logging.Formatter(
+        '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S'
+    )
+    
+    # Console handler
+    console_handler = logging.StreamHandler(sys.stdout)
+    console_handler.setLevel(level)
+    console_handler.setFormatter(formatter)
+    logger.addHandler(console_handler)
+    
+    return logger
+
+# Default logger instance
+logger = setup_logger()
