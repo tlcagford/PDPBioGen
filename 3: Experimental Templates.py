a707b600b0d03b11aad3279c3810d98b9e70786b
Tony E Ford <tlcagford@gmail.com>
2025-11-21 06:10:52 -0700

    Create 3: Experimental Templates.py

---
+++ 3: Experimental Templates.py
@@ -0,0 +1,7 @@
+# Pre-configured experimental designs
+EXPERIMENT_TEMPLATES = {
+    "rnaseq_time_series": RNASeqTimeSeriesTemplate(),
+    "qpcr_dose_response": QPCRDoseResponseTemplate(),
+    "proteomics_case_control": ProteomicsCaseControlTemplate(),
+    "multiomics_integration": MultiOmicsIntegrationTemplate()
+}
