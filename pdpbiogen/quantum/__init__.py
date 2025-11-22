906c33ce0dc7a404c4e6e629ff6128acce8ad9a3
Tony E Ford <tlcagford@gmail.com>
2025-11-21 19:32:48 -0700

    Update and rename .txt to __init__.py

---
+++ pdpbiogen/quantum/__init__.py
@@ -0,0 +1,17 @@
+# Quantum BCI Drop-In for PDPBioGen
+# Integrates primordial photon-dark photon entanglement for non-contact neural symmetry mapping
+
+from .mapper import (
+    NeuroSymmetryAgent,
+    SymmetryOptimizer,
+    DarkPhotonEntangler,
+    QuantumNeuroSymmetryMapper
+)
+
+__version__ = '0.1.0'
+__all__ = [
+    'NeuroSymmetryAgent',
+    'SymmetryOptimizer',
+    'DarkPhotonEntangler',
+    'QuantumNeuroSymmetryMapper'
+]
