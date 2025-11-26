a0eefd8fd615fe45189341f852b0d699a53d2fa2
Tony E Ford <tlcagford@gmail.com>
2025-11-26 14:21:27 -0700

    Create Integration into PDPBioGen.py

---
+++ neuro/Integration into PDPBioGen.py
@@ -0,0 +1,7 @@
+from dpbiogen.neuro.neuro_symmetry_mapper import NeuroSymmetryMapper
+
+nsm = NeuroSymmetryMapper()
+brain_state = nsm.compute(eeg_path)
+
+gene_delta = gene_mapper.map(brain_state)
+flux_results = metabolism_engine.run(gene_delta)
