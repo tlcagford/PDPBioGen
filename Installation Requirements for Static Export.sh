2900a575a0d50b4547d68cbf6a7a8831acfadcda
Tony E Ford <tlcagford@gmail.com>
2025-11-26 11:47:33 -0700

    Create Installation Requirements for Static Export.sh

---
+++ Installation Requirements for Static Export.sh
@@ -0,0 +1,6 @@
+# Additional dependencies for static export
+pip install kaleido  # Required for static plot export
+pip install plotly>=5.0.0
+
+# Existing requirements still needed
+pip install biopython pandas plotly
